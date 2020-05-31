ca2b2a90306d0e70cc04c2dc245135360107ca98
user <mrupvkumar@gmail.com>
2020-05-31 16:35:10 +0530

    working kafka producer code

--- Stream_Producer.py
+++ Stream_Producer.py
@@ -1,13 +1,13 @@
-from kafka import KafkaProducer
+from kafka import KafkaProducer as kp
 import csv
 import time
 import json
 
-producer = KafkaProducer(bootstrap_servers = 'localhost:9092')
+producer = kp(bootstrap_servers = 'localhost:9092')
 
 with open('C:/Users/mrupv/bits/spa/Assignment2/paysim1/PS_20174392719_1491204439457_log.csv','rt') as file:
     data = list(csv.DictReader(file))
     for row in data:
         print(json.dumps(row))
-        producer.send(topic='fraud_log',value=json.dumps(row))
+        producer.send(topic='fraud_log',value=json.dumps(row).encode('utf-8'))
         time.sleep(1)
