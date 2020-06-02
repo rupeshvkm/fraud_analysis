7c073d2d7f508c7a5be5d4b4b4faab309544d5af
user <mrupvkumar@gmail.com>
2020-06-02 17:43:17 +0530

    stream code

--- Stream_Producer.py
+++ Stream_Producer.py
@@ -1,13 +1,14 @@
-from kafka import KafkaProducer as kp
+from kafka import KafkaProducer
 import csv
 import time
 import json
+from json import dumps
 
-producer = kp(bootstrap_servers = 'localhost:9092')
+producer = KafkaProducer(bootstrap_servers = ['localhost:9092'],value_serializer=lambda x: dumps(x).encode('utf-8'))
 
 with open('C:/Users/mrupv/bits/spa/Assignment2/paysim1/train.csv','rt') as file:
     data = list(csv.DictReader(file))
     for row in data:
         print(json.dumps(row))
-        producer.send(topic='fraud_log',value=json.dumps(row).encode('utf-8'))
+        producer.send(topic='fraud.logs',value=row)
         time.sleep(3)
--- spark_consumer.py
+++ spark_consumer.py
@@ -1,6 +1,7 @@
 from pyspark.sql import SparkSession
-from pyspark.streaming import StreamingContext
-from pyspark.streaming.kafka import KafkaUtils
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+
 
 spark = SparkSession.builder.master("local[5]").appName("stream_processor").getOrCreate()
 # sc = SparkContext(appName="PythonStreamingKafkaWordCount")
@@ -10,9 +11,31 @@ linesDF = spark \
     .readStream \
     .format("kafka") \
     .option("kafka.bootstrap.servers", "localhost:9092") \
-    .option("subscribe", "fraud_log") \
-    .option("startingOffsets", "earliest") \
+    .option("subscribe", "fraud.logs") \
     .load()
+    #.option("startingOffsets", "earliest") \
 
-query = linesDF.writeStream.outputMode("append").format("console").start()
+schema = StructType([
+    StructField("step", StringType(), True),
+    StructField("type", StringType(), True),
+    StructField("amount", StringType(), True),
+    StructField("nameOrig", StringType(), True),
+    StructField("oldbalanceOrg", StringType(), True),
+    StructField("newbalanceOrig", StringType(), True),
+    StructField("nameDest", StringType(), True),
+    StructField("oldbalanceDest", StringType(), True),
+    StructField("newbalanceDest", StringType(), True),
+    StructField("isFraud", StringType(), True),
+    StructField("isFlaggedFraud", StringType(), True)
+])
+
+rowDF = linesDF.withColumn('value',from_json(linesDF['value'].cast('string'),schema))\
+    .select('value.step','value.type','value.amount','value.nameOrig','value.oldbalanceOrg','value.newbalanceOrig','value.nameDest','value.oldbalanceDest','value.newbalanceDest','value.isFraud','value.isFlaggedFraud')
+
+query = rowDF.writeStream\
+    .outputMode("append")\
+    .format("csv")\
+    .option("path","C:/Users/mrupv/bits/spa/Assignment2/project/data/")\
+    .option("checkpointLocation", "C:/Users/mrupv/bits/spa/Assignment2/project/checkpoint_path/")\
+    .start()
 query.awaitTermination()
